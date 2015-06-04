1ceb5f76fc60030ee20907851411680ab2b687c2
michielvanveldhuizen <balmungak@hotmail.com>
2015-06-04 09:50:26 +0300

    en de Mapping/MailErrorMap.cs

---
+++ Percurrentis/Mapping/MailErrorMap.cs
@@ -0,0 +1,20 @@
+﻿// <copyright company=CSi Romania SRL>
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Michiel van Veldhuizen</author>
+// <summary>Mapping classes for the migration to the database</summary>
+
+using Percurrentis.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Percurrentis.Mapping
+{
+    public class MailErrorMap : EntityTypeConfiguration<MailError>
+    {
+        public MailErrorMap()
+        {
+            this.HasKey(p => p.Id);
+        }
+    }
+}
