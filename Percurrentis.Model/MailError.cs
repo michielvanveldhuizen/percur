49587670f4f14300f9065208161a7ec1827b4c34
michielvanveldhuizen <balmungak@hotmail.com>
2015-06-04 09:47:49 +0300

    missing mail error.cs

---
+++ Percurrentis.Model/MailError.cs
@@ -0,0 +1,21 @@
+﻿// <copyright company=CSi Romania SRL>
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Michiel van Veldhuizen</author>
+// <summary>Model classes for the database</summary>
+
+using Percurrentis.Model.Validation.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Percurrentis.Model
+{
+    public class MailError
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+        public string ToEmail { get; set; }
+        public string TypeOfEmail { get; set; }
+    }
+}
